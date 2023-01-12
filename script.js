1f54fd0bfb12ee73c557d18f006ac8a5bd317681
TienCG <tiencuongkhoa71@gmail.com>
2023-01-12 19:53:15 +0700

    tien commit

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const complete = document.getElementById("complete");
 let correct = true;
 
 quoteInput.addEventListener('input', ()=>{
-    const arrayDisplay =  quoteDisplay.querySelectorAll('span')
-    const arrayInput =  quoteInput.value.split('')
+    const arrayDisplay = quoteDisplay.querySelectorAll('span')
+    const arrayInput = quoteInput.value.split(' ')
     arrayDisplay.forEach((characterSpan, index)=>{
+        console.log(arrayInput[index]);
         const character = arrayInput[index];
+        console.log(characterSpan.innerText);
+        console.log(character+" " == characterSpan.innerText);
         if(character == null){
             characterSpan.classList.remove('correct')
             characterSpan.classList.remove('incorrect')
             correct = false;
-        } else if(character == characterSpan.innerText){
+        } else if(character+" " == characterSpan.innerText){
             characterSpan.classList.add('correct')
             characterSpan.classList.remove('incorrect')
             correct = true;
@@ -41,10 +44,10 @@ async function getNextQuote(){
     complete.innerText = "";
     console.log(quote);
     quoteDisplay.innerHTML='';
-    quote.split('').forEach(character => {
+    quote.split(' ').forEach(character => {
         const characterSpan = document.createElement('span')
         // characterSpan.classList.add('correct');
-        characterSpan.innerText = character;
+        characterSpan.innerText = character+" ";
         quoteDisplay.appendChild(characterSpan)
     })
     quoteInput.value = null;
